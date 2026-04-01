d9db868a56366d3b25b0cb6645b36017cf901e23
JoaquinCelentano <jcelentanoalonso@udesa.edu.ar>
2026-04-01 15:45:59 -0300

    traje la maqueta base para empezar a migrar los html a componentes react

--- progra3pi/progra3pi/src/index.js
+++ progra3pi/progra3pi/src/index.js
@@ -3,14 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from "react-router-dom"; // 👈 agregar esto
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>   
-      <App />
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
 
@@ -18,6 +15,3 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-
-
-
